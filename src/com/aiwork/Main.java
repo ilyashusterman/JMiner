5a2e813a31aafc6c6e8c61118ec19d088ab797f8
ilya shusterman <ilyashusterman@outlook.com>
2017-10-06 15:00:20 +0300

    Initial commit

---
+++ src/com/aiwork/Main.java
@@ -0,0 +1,22 @@
+package com.aiwork;
+import dnl.utils.text.table.TextTable;
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        startPrints();
+        Miner.start();
+    }
+
+    private static void startPrints() {
+        logger.info("Starting");
+        String []columnNames = {"Properties"};
+        String [][]data = {{"some specifications..."}};
+        TextTable tt = new TextTable(columnNames, data);
+        tt.printTable();
+    }
+
+}
---
+++ src/com/aiwork/Miner.java
@@ -0,0 +1,37 @@
+package com.aiwork;
+import dnl.utils.text.table.TextTable;
+import org.apache.log4j.Logger;
+
+public class Miner {
+
+    public static void start(){
+        Miner miner = new Miner();
+        //TODO connect to bl server
+        miner.connectToRemoteServer();
+        //TODO using processor resources
+        miner.useAllowedProcessor();
+        // looping till exit
+        //TODO generate HashData
+        miner.generateHashData();
+        //TODO send answers to server
+        miner.sendDataToRemote();
+        //TODO print Reciver data
+        miner.printEndingRemoteData();
+    }
+
+    private void printEndingRemoteData() {
+    }
+
+    private void sendDataToRemote() {
+    }
+
+    private void generateHashData() {
+    }
+
+    private void useAllowedProcessor() {
+    }
+
+    private void connectToRemoteServer() {
+    }
+
+}
